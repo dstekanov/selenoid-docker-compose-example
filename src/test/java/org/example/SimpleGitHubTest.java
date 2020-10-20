17cc851c4608392a7d0a385ba33f26d7097c4607
Dmytro Stekanov <dstekanov.tech@gmail.com>
2020-10-20 15:41:56 +0300

    fill test

--- src/test/java/org/example/SimpleGitHubTest.java
+++ src/test/java/org/example/SimpleGitHubTest.java
@@ -1,4 +1,32 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
 public class SimpleGitHubTest {
+
+    @Test
+    public void testIssue() {
+        step("Open main page", () -> {
+            open("https://github.com");
+        });
+        step("Go to repository page", () -> {
+            $x("//*[contains(@class, 'header-search-input')]").click();
+            $x("//*[contains(@class, 'header-search-input')]")
+                    .sendKeys("dstekanov/selenoid-docker-compose-example");
+            $x("//*[contains(@class, 'header-search-input')]").submit();
+            $x("//a[@href='/dstekanov/selenoid-docker-compose-example']").click();
+        });
+        step("Verify About title", () -> {
+            $("[href='/dstekanov/selenoid-docker-compose-example']").click();
+            $(".BorderGrid-row .f4").shouldHave(text("Hello, Selenoid!"));
+        });
+
+    }
 }
